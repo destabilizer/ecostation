640c79ab62b0cf374588cdc070f9177ae2f223c4
Balaram Usov <dest@disroot.org>
2019-05-13 15:22:44 +0300

    Code style

--- datacapture.py
+++ datacapture.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 '''
-This is a simple script that captures data from datchiki and send it to the ecostatus server.
+This is a simple script that captures data from sensors and send it to the ecostatus server.
 '''
 
 import json
@@ -15,8 +15,8 @@ def send_data(dictdata, sourcename, full_address):
     req = requests.post(full_address, json=dictdata)
     return (req.status_code, req.reason)
 
-def get_data():
-    return {"temp": 20, "dust": 0.4}
+def fake_get_data():
+    return {"temp": 20, "dust": 0.4} 
 
 def mainloop(sourcename, full_address):
     d = get_data()
@@ -26,6 +26,8 @@ def mainloop(sourcename, full_address):
         print("Unseccessful POST")
         
 def maintest():
+    global get_data
+    get_data = fake_get_data
     while True:
         mainloop("testclient", "http://127.0.0.1:8080/")
         sleep(1)
