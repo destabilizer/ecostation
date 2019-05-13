8d8e3ab903d06879d296297d97617693d1fa90d6
Balaram Usov <dest@disroot.org>
2019-05-13 15:19:49 +0300

    Http send fix

--- datacapture.py
+++ datacapture.py
@@ -5,29 +5,29 @@ This is a simple script that captures data from datchiki and send it to the ecos
 '''
 
 import json
-import urllib2
+import requests
 from time import sleep
 
-def send_data(dictdata, sourcename, address, port):
+def send_data(dictdata, sourcename, full_address):
     dictdata["type"] = "data"
     dictdata["source"] = sourcename
-    
-    req = urllib2.Request(str(address)+":"+str(port))
-    req.add_header('Content-Type', 'application/json')
 
-    response = urllib2.urlopen(req, json.dumps(dictdata))
-    return response
+    req = requests.post(full_address, json=dictdata)
+    return (req.status_code, req.reason)
 
 def get_data():
     return {"temp": 20, "dust": 0.4}
 
-def mainloop(sourcename, address, port):
+def mainloop(sourcename, full_address):
     d = get_data()
-    send_data(d, sourcename, address, port)
-
+    try:
+        send_data(d, sourcename, full_address)
+    except:
+        print("Unseccessful POST")
+        
 def maintest():
     while True:
-        mainloop("testclient", "127.0.0.1", 8080)
+        mainloop("testclient", "http://127.0.0.1:8080/")
         sleep(1)
     
 if __name__=="__main__":
